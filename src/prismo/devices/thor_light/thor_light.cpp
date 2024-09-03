40e1c7ab633b8a022f4c3615d34ea4c8f30728b5
Karl-Krauth <karl.krauth@gmail.com>
2024-09-03 12:53:02 -0700

    Add device name finder.

--- src/prismo/devices/thor_light/thor_light.cpp
+++ src/prismo/devices/thor_light/thor_light.cpp
@@ -1,10 +1,36 @@
+#include <stdexcept>
+#include <ranges>
+#include <string>
+#include <vector>
+
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-int add(int i, int j) { return i + j; }
+#include "TLUP.h"
 
-namespace py = pybind11;
+
+std::vector<std::string> devices() {
+    ViStatus err;
+    ViUInt32 num_rsrc;
+    err = TLUP_findRsrc(0, &num_rsrc);
+    if (err != VI_SUCCESS) {
+         throw std::runtime_error("Could not find devices. Error code: " + std::to_string(err)); 
+    }
+
+    std::vector<std::string> names;
+    for (int i=0; i < num_rsrc; ++i) {
+	ViChar name[512];
+	err = TLUP_getRsrcName(0, i, name);
+	if (err != VI_SUCCESS) {
+            throw std::runtime_error("Could not get device name. Error code: " + std::to_string(err));
+	}
+        names.push_back(name);
+    }
+
+    return names;
+}
 
 PYBIND11_MODULE(thor_light, m) {
-  m.def("add", &add);
-  m.def("subtract", [](int i, int j) { return i + j; });
+    m.def("devices", &devices);
+    m.def("subtract", [](int i, int j) { return i + j; });
 }
