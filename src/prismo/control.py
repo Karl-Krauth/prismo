fffdd6a7f5d530155b173827679ed2717627abe9
Karl Krauth <karl.krauth@gmail.com>
2024-09-03 16:01:25 -0700

    Update mux/minichip for new designs.

--- src/prismo/control.py
+++ src/prismo/control.py
@@ -658,62 +658,90 @@ class Valves:
 class Mux:
     def __init__(self, name, mapping, valves):
         self.name = name
-        num_bits = (len(mapping) - 2) // 2
+        num_bits = (len(mapping) - 4) // 2
         self._zeros = [mapping[f"{i}_0"] for i in reversed(range(num_bits))]
         self._ones = [mapping[f"{i}_1"] for i in reversed(range(num_bits))]
+        self._input = mapping["input"]
+        self._all_inputs = self._zeros + self._ones + [self._input]
+        self._purge = mapping["purge"]
+
+        self._flow = mapping["flow"]
         self._waste = mapping["waste"]
-        self._io = mapping["io"]
-        self._all = self._zeros + self._ones + [self._waste, self._io]
         self._valves = valves
 
     @property
-    def state(self):
-        waste_state = 1 - self._valves[self._waste]
-        io_state = 1 - self._valves[self._io]
+    def input(self):
+        purge_state = 1 - self._valves[self._purge]
+        input_state = 1 - self._valves[self._input]
         zeros_state = np.array([1 - self._valves[v] for v in self._zeros])
         ones_state = np.array([1 - self._valves[v] for v in self._ones])
-        all_state = np.array([1 - self._valves[v] for v in self._all])
-        if np.all(all_state):
+        all_state = np.array([1 - self._valves[v] for v in self._all_inputs])
+        if np.all(all_state) and not purge_state:
             return "open"
-        elif not np.any(all_state):
+        elif not np.any(all_state) and not purge_state:
             return "closed"
-        elif waste_state and not io_state and not np.any(zeros_state) and not np.any(ones_state):
-            return "waste"
-        elif np.all(zeros_state + ones_state == 1) and io_state:
+        elif purge_state and not input_state:
+            return "purge"
+        elif np.all(zeros_state + ones_state == 1) and input_state and not purge_state:
             return sum(b * 2**i for i, b in enumerate(reversed(ones_state)))
         else:
             return "invalid"
 
-    @state.setter
-    def state(self, new_state):
+    @input.setter
+    def input(self, new_state):
+        for v in self._all_inputs:
+            self._valves[v] = 1
+        self._valves[self._purge] = 1
         if new_state == "open":
-            for v in self._all:
+            for v in self._all_inputs:
                 self._valves[v] = 0
         elif new_state == "closed":
-            for v in self._all:
-                self._valves[v] = 1
-        elif new_state == "waste":
-            for v in self._all:
-                self._valves[v] = 1
-            self._valves[self._waste] = 0
+            pass
+        elif new_state == "purge":
+            for v in self._zeros:
+                self._valves[v] = 0
+            for v in self._ones:
+                self._valves[v] = 0
+            self._valves[self._purge] = 0
         elif isinstance(new_state, int):
-            for v in self._all:
-                self._valves[v] = 1
             for i, b in enumerate(bin(new_state)[2:].zfill(len(self._ones))):
                 if b == "0":
                     self._valves[self._zeros[i]] = 0
                 else:
                     self._valves[self._ones[i]] = 0
-            self._valves[self._io] = 0
-        elif "waste" in new_state:
-            new_state = int(new_state.split("_")[0])
-            for v in self._all:
-                self._valves[v] = 1
+            self._valves[self._input] = 0
+        elif "purge" in new_state:
+            new_state = int(new_state.split("_")[1])
             for i, b in enumerate(bin(new_state)[2:].zfill(len(self._ones))):
                 if b == "0":
                     self._valves[self._zeros[i]] = 0
                 else:
                     self._valves[self._ones[i]] = 0
+            self._valves[self._purge] = 0
+
+    @property
+    def output(self):
+        waste_state = 1 - self._valves[self._waste]
+        flow_state = 1 - self._valves[self._flow]
+        if waste_state and flow_state:
+            return "open"
+        elif waste_state:
+            return "waste"
+        elif flow_state:
+            return "flow"
+        else:
+            return "closed"
+
+    @output.setter
+    def output(self, new_state):
+        self._valves[self._waste] = 1
+        self._valves[self._flow] = 1
+        if new_state == "waste":
+            self._valves[self._waste] = 0
+        elif new_state == "flow":
+            self._valves[self._flow] = 0
+        elif new_state == "open":
+            self._valves[self._flow] = 0
             self._valves[self._waste] = 0
 
 
@@ -725,6 +753,7 @@ class MiniChip:
         self._ones = [mapping[f"{i}_1"] for i in reversed(range(num_bits))]
         self._all_io = self._zeros + self._ones
         self._buttons = mapping["buttons"]
+        self._out = mapping["out"]
         self._sandwiches = mapping["sandwiches"]
         self._valves = valves
 
@@ -770,6 +799,17 @@ class MiniChip:
             self._valves[self._buttons] = "off"
         else:
             self._valves[self._buttons] = "on"
+
+    @property
+    def output(self):
+        return "closed" if self._valves[self._out] else "open"
+
+    @output.setter
+    def output(self, new_state):
+        if new_state == "open" or not new_state:
+            self._valves[self._out] = "off"
+        else:
+            self._valves[self._out] = "on"
 
     @property
     def snw(self):
