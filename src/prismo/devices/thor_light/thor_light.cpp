1c968d7640481550e433a366ec9ea4180ab6869b
Karl-Krauth <karl.krauth@gmail.com>
2024-09-03 15:21:05 -0700

    Add info function for thor light.

--- src/prismo/devices/thor_light/thor_light.cpp
+++ src/prismo/devices/thor_light/thor_light.cpp
@@ -1,6 +1,7 @@
 #include <mutex>
 #include <stdexcept>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include <pybind11/pybind11.h>
@@ -14,15 +15,15 @@ std::mutex mutex;
 
 std::vector<std::string> devices() {
     const std::lock_guard<std::mutex> lock(mutex);
-    ViStatus err;
-    ViUInt32 num_rsrc;
-    err = TLUP_findRsrc(0, &num_rsrc);
+    int err;
+    unsigned int num_rsrc;
+    err = TLUP_findRsrc(0, reinterpret_cast<ViPSession>(&num_rsrc));
     if (err != VI_SUCCESS) {
          throw std::runtime_error("Could not find devices. Error code: " + std::to_string(err)); 
     }
 
     std::vector<std::string> names;
-    for (int i=0; i < num_rsrc; ++i) {
+    for (int i = 0; i < num_rsrc; ++i) {
 	ViChar name[512];
 	err = TLUP_getRsrcName(0, i, name);
 	if (err != VI_SUCCESS) {
@@ -35,10 +36,10 @@ std::vector<std::string> devices() {
 }
 
 
-unsigned int init(std::string port) {
+unsigned int init_device(std::string port) {
     const std::lock_guard<std::mutex> lock(mutex);
     unsigned int device_id;
-    ViStatus err = TLUP_init(port, VI_FALSE, VI_FALSE, &device_id);
+    int err = TLUP_init(port.data(), VI_FALSE, VI_FALSE, reinterpret_cast<ViPSession>(&device_id));
     if (err != VI_SUCCESS) {
          throw std::runtime_error("Could not initialize device. Error code: " + std::to_string(err)); 
     }
@@ -47,17 +48,33 @@ unsigned int init(std::string port) {
 }
 
 
-void close(unsigned int device_id) {
+void close_device(int device_id) {
     const std::lock_guard<std::mutex> lock(mutex);
-    ViStatus err = TLUP_close(device_id);
+    int err = TLUP_close(device_id);
     if (err != VI_SUCCESS) {
          throw std::runtime_error("Could not close device. Error code: " + std::to_string(err)); 
     }
 }
 
 
+std::tuple<std::string, std::string, double, double, double>
+info(uint32_t device_id) {
+    const std::lock_guard<std::mutex> lock(mutex);
+    char name[256];
+    char serial_num[256];
+    ViReal64 ampere_limit, volt_limit, wavelength;
+    int err = TLUP_getLedInfo(device_id, name, serial_num, &ampere_limit, &volt_limit, &wavelength);
+    if (err != VI_SUCCESS) {
+         throw std::runtime_error("Could not get device info. Error code: " + std::to_string(err)); 
+    }
+
+    return {name, serial_num, ampere_limit, volt_limit, wavelength};
+}
+
+
 PYBIND11_MODULE(thor_light, m) {
     m.def("devices", &devices);
-    m.def("init", &init);
-    m.def("close", &close);
+    m.def("init", &init_device);
+    m.def("close", &close_device);
+    m.def("info", &info);
 }
