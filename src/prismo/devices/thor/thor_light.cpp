2da7c0a07dd2cc9d0a8320a4b2f49b54d7819e0e
Karl Krauth <karl.krauth@gmail.com>
2024-09-03 18:19:37 -0700

    Move thor light to generic thor folder.

--- src/prismo/devices/thor/thor_light.cpp
+++ src/prismo/devices/thor/thor_light.cpp
@@ -19,16 +19,16 @@ std::vector<std::string> devices() {
     unsigned int num_rsrc;
     err = TLUP_findRsrc(0, reinterpret_cast<ViPSession>(&num_rsrc));
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not find devices. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not find devices. Error code: " + std::to_string(err));
     }
 
     std::vector<std::string> names;
     for (int i = 0; i < num_rsrc; ++i) {
-	ViChar name[512];
-	err = TLUP_getRsrcName(0, i, name);
-	if (err != VI_SUCCESS) {
-            throw std::runtime_error("Could not get device name. Error code: " + std::to_string(err));
-	}
+    ViChar name[512];
+    err = TLUP_getRsrcName(0, i, name);
+        if (err != VI_SUCCESS) {
+                throw std::runtime_error("Could not get device name. Error code: " + std::to_string(err));
+        }
         names.push_back(name);
     }
 
@@ -41,7 +41,7 @@ unsigned int init_device(std::string port) {
     unsigned int device_id;
     int err = TLUP_init(port.data(), VI_FALSE, VI_FALSE, reinterpret_cast<ViPSession>(&device_id));
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not initialize device. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not initialize device. Error code: " + std::to_string(err));
     }
 
     return device_id;
@@ -52,7 +52,7 @@ void close_device(unsigned int device_id) {
     const std::lock_guard<std::mutex> lock(mutex);
     int err = TLUP_close(device_id);
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not close device. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not close device. Error code: " + std::to_string(err));
     }
 }
 
@@ -65,7 +65,7 @@ info(unsigned int device_id) {
     double ampere_limit, volt_limit, wavelength;
     int err = TLUP_getLedInfo(device_id, name, serial_num, &ampere_limit, &volt_limit, &wavelength);
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not get device info. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not get device info. Error code: " + std::to_string(err));
     }
 
     return {name, serial_num, ampere_limit, volt_limit, wavelength};
@@ -73,10 +73,11 @@ info(unsigned int device_id) {
 
 
 double get_amps(unsigned int device_id) {
+    const std::lock_guard<std::mutex> lock(mutex);
     double amps;
     int err = TLUP_getLedCurrentSetpoint(device_id, TLUP_ATTR_SET_VAL, &amps);
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not get device current. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not get device current. Error code: " + std::to_string(err));
     }
 
     return amps;
@@ -84,17 +85,19 @@ double get_amps(unsigned int device_id) {
 
 
 void set_amps(unsigned int device_id, double amps) {
+    const std::lock_guard<std::mutex> lock(mutex);
     int err = TLUP_setLedCurrentSetpoint(device_id, amps);
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not set device current. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not set device current. Error code: " + std::to_string(err));
     }
 }
 
 
 void set_output(unsigned int device_id, bool enable) {
+    const std::lock_guard<std::mutex> lock(mutex);
     int err = TLUP_switchLedOutput(device_id, enable);
     if (err != VI_SUCCESS) {
-         throw std::runtime_error("Could not set device output. Error code: " + std::to_string(err)); 
+         throw std::runtime_error("Could not set device output. Error code: " + std::to_string(err));
     }
 }
 
