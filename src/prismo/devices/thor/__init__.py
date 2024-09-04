f0fec193611ee8022e4f3e1432c7b5c68cdd8a0b
Karl Krauth <karl.krauth@gmail.com>
2024-09-04 11:24:05 -0700

    Only try to compile the thor library on windows.

--- src/prismo/devices/thor/__init__.py
+++ src/prismo/devices/thor/__init__.py
@@ -1,6 +1,7 @@
 import os
 
-# Enable local dll loading for editable pip installs.
-os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
-
-from .thor import Light
+# Thorlabs drivers are only supported in Windows.
+if os.name == "nt":
+    # Enable local dll loading for editable pip installs.
+    os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
+    from .thor import Light
