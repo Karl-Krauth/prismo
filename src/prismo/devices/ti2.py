d792afcd41b5f246c6f0e32adde2b357ba670959
Karl Krauth <karl.krauth@gmail.com>
2025-01-09 21:08:15 -0800

    Update lightpath names for ti2.

--- src/prismo/devices/ti2.py
+++ src/prismo/devices/ti2.py
@@ -1,7 +1,6 @@
 class Filter:
     def __init__(self, name, core, filter, states=None):
         self.name = name
-        self.states = states
         self._core = core
         if "ti2_scope" not in core.getLoadedDevices():
             core.loadDevice("ti2_scope", "NikonTi2", "Ti2-E__0")
@@ -14,6 +13,7 @@ class Filter:
         if states is None:
             self.states = [i for i in range(n_states)]
         else:
+            self.states = states
             if len(self.states) < n_states:
                 raise ValueError(
                     f"{name} requires {n_states} states (not {len(self.states)}) to be specified."
@@ -42,7 +42,6 @@ class Filter:
 class LightPath:
     def __init__(self, name, core, states=None):
         self.name = name
-        self.states = states
         self._core = core
         if "ti2_scope" not in core.getLoadedDevices():
             core.loadDevice("ti2_scope", "NikonTi2", "Ti2-E__0")
@@ -51,15 +50,13 @@ class LightPath:
         core.setParentLabel(name, "ti_scope")
         core.initializeDevice(name)
 
-        if states is None:
-            self.states = ["eye", "l100", "r100", "l80"]
-        else:
-            if len(self.states) != 4:
-                raise ValueError(
-                    f"{name} requires 4 states (not {len(self.states)}) to be specified."
-                )
-            for i, state in enumerate(self.states):
-                self._core.defineStateLabel(name, i, state)
+        self.states = ["eye", "right", "aux", "left"] if states is None else states
+        if len(self.states) != 4:
+            raise ValueError(
+                f"{name} requires 4 states (not {len(self.states)}) to be specified."
+            )
+        for i, state in enumerate(self.states):
+            self._core.defineStateLabel(name, i, state)
 
     def wait(self):
         self._core.waitForDevice(self.name)
@@ -82,7 +79,6 @@ class LightPath:
 class Objective:
     def __init__(self, name, core, zooms, states=None):
         self.name = name
-        self.states = states
         self._core = core
         if "ti2_scope" not in core.getLoadedDevices():
             core.loadDevice("ti2_scope", "NikonTi2", "Ti2-E__0")
@@ -95,6 +91,7 @@ class Objective:
         if states is None:
             self.states = [i for i in range(n_states)]
         else:
+            self.states = states
             if len(self.states) < n_states:
                 raise ValueError(
                     f"{name} requires {n_states} states (not {len(self.states)}) to be specified."
