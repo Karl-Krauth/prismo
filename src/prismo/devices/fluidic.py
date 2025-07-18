f47396f359973ceada110e1e77f0da9d9c734d59
Karl Krauth <karl.krauth@gmail.com>
2025-07-18 13:05:31 -0700

    Consolidate all sipper functions into one class.

--- src/prismo/devices/fluidic.py
+++ src/prismo/devices/fluidic.py
@@ -4,13 +4,22 @@ import struct
 
 import numpy as np
 
-
-class Handler:
-    def __init__(self, name, port, x_max=7500, y_max=6000, z_max=3000):
+class Sipper:
+    def __init__(self, name, cnc_port, pump_port, valve_port, a1_pos, h12_pos, z_bottom, mapping=None, x_max=7500, y_max=6000, z_max=3000):
         self.name = name
-        self._socket = serial.Serial(port, baudrate=9600)
+        self._cnc_socket = serial.Serial(cnc_port, baudrate=9600)
+        self._valve_socket = serial.Serial(valve_port, baudrate=9600)
+        self._pump_socket = serial.Serial(pump_port, baudrate=9600)
         self._xyz = np.zeros(3)
         self._max = np.array([x_max, y_max, z_max])
+        self._valve = 1
+        self._frequency = 0
+        self._voltage = 0
+        self._a1 = a1_pos
+        self._h12 = h12_pos
+        self._z_bottom = z_bottom
+        self._well = "none"
+        self._mapping = mapping if mapping is not None else {}
         self.home()
 
     @property
@@ -25,8 +34,8 @@ class Handler:
         self._xyz = value
         value = (value * self._max).astype(np.uint32)
         msg = struct.pack("<BIII", 1, *value)
-        self._socket.write(msg)
-        self._socket.read(1)
+        self._cnc_socket.write(msg)
+        self._cnc_socket.read(1)
 
     @property
     def x(self):
@@ -54,36 +63,74 @@ class Handler:
 
     def home(self):
         msg = struct.pack("<B", 0)
-        self._socket.write(msg)
-        self._socket.read(1)
+        self._cnc_socket.write(msg)
+        self._cnc_socket.read(1)
         self._xyz = np.zeros(3)
 
-
-class Plate96:
-    def __init__(self, name, handler, a1_pos, h12_pos, z_bottom, mapping=None):
-        self.name = name
-        self._handler = handler
-        self._a1 = a1_pos
-        self._h12 = h12_pos
-        self._z_bottom = z_bottom
-        self._position = "none"
-        self._mapping = mapping if mapping is not None else {}
+    @property
+    def flow_rate(self):
+        msg = struct.pack("<B", 2)
+        self._valve_socket.write(msg)
+        error = self._valve_socket.read(1)
+        if error != b"\x00":
+            raise RuntimeError(read_byte_str(self._valve_socket))
+        flow = struct.unpack("<f", self._valve_socket.read(4))[0]
+        temperature, flags = struct.unpack("<fH", self._valve_socket.read(6))
+        print(temperature, flags)
+        return flow
 
     @property
-    def state(self):
-        return self._state
+    def valve(self):
+        return self._valve
 
-    @state.setter
-    def state(self, new_state):
-        if new_state == "none":
-            self._handler.z = 0
-            self._state = "none"
+    @valve.setter
+    def valve(self, value):
+        value = 1 if value in [1, "open"] else 0
+        self._valve = value
+        msg = struct.pack("<B", value)
+        self._valve_socket.write(msg)
+        self._valve_socket.read(1)
+
+    @property
+    def frequency(self):
+        return self._frequency
+
+    @frequency.setter
+    def frequency(self, value):
+        if value > 800 or value < 0:
+            raise ValueError("Frequency must be less than or equal to 800 Hz.")
+        self._frequency = value
+        self._pump_socket.write(struct.pack("<HB", self._frequency, self._voltage))
+        self._pump_socket.read(1)
+
+    @property
+    def voltage(self):
+        return self._voltage
+
+    @voltage.setter
+    def voltage(self, value):
+        self._voltage = value
+        self._pump_socket.write(struct.pack("<HB", self._frequency, self._voltage))
+        self._pump_socket.read(1)
+
+    @property
+    def well(self):
+        return self._well
+
+    @well.setter
+    def well(self, new_well):
+        if new_well == "none":
+            self.z = 0
+            self._well = "none"
             return
 
-        if new_state in self._mapping:
-            pos = self._mapping[new_state]
+        if new_well in self._mapping:
+            pos = self._mapping[new_well]
         else:
-            pos = new_state
+            pos = new_well
+
+        if new_well == self._well:
+            return
 
         if not re.fullmatch(r"[A-H]1?[0-9]", pos) or int(pos[1:]) > 12:
             raise ValueError("Plate position must be in the format [A-Z][1-12].")
@@ -102,7 +149,33 @@ class Plate96:
             row = 7 - row
             y = self._h12[1] + row * (self._a1[1] - self._h12[1]) / 7
 
-        self._handler.z = 0
-        self._handler.xyz = (x, y, 0)
-        self._handler.z = self._z_bottom
-        self._state = new_state
+        self.z = 0
+        self.xyz = (x, y, 0)
+        self.z = self._z_bottom
+        self._well = new_well
+
+    def sip(self, well):
+        self.frequency = 0
+        self.voltage = 0
+        self.well = well
+        self.valve = "closed"
+        self.frequency = 200
+        self.voltage = 250
+
+    def purge(self, well):
+        self.frequency = 0
+        self.voltage = 0
+        self.well = well
+        self.valve = "open"
+        self.frequency = 200
+        self.voltage = 250
+
+
+def read_byte_str(socket):
+    received_bytes = bytearray()
+    while True:
+        byte = socket.read(1)
+        if not byte or byte == b"\x00":
+            break
+        received_bytes.extend(byte)
+    return received_bytes.decode("ascii")
