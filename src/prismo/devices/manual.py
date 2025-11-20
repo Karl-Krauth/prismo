2c6b23b922c8dc25e754e49c6b9ffe0b6323bee0
Karl Krauth <karl.krauth@gmail.com>
2025-11-20 11:56:20 -0800

    Remove name clash.

--- src/prismo/devices/manual.py
+++ src/prismo/devices/manual.py
@@ -1,19 +1,19 @@
 class Objective:
     def __init__(self, name, zoom=1):
         self.name = name
-        self.zoom = zoom
+        self._zoom = zoom
 
     def wait(self):
         pass
 
     @property
     def state(self):
-        return self.zoom
+        return self._zoom
 
     @state.setter
     def state(self, new_state):
-        self.zoom = new_state
+        self._zoom = new_state
 
     @property
     def zoom(self):
-        return self.zoom
+        return self._zoom
